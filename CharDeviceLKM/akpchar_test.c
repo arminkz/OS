f29875bbb1124797275aeb199a8293f60cee5969
Armin Kazemi <arminkz@live.com>
2018-01-05 06:12:55 -0800

    Updated Test Snippet

--- CharDeviceLKM/akpchar_test.c
+++ CharDeviceLKM/akpchar_test.c
@@ -1,13 +1,3 @@
-/**
- * @file   testebbchar.c
- * @author Derek Molloy
- * @date   7 April 2015
- * @version 0.1
- * @brief  A Linux user space program that communicates with the ebbchar.c LKM. It passes a
- * string to the LKM and reads the response from the LKM. For this example to work the device
- * must be called /dev/ebbchar.
- * @see http://www.derekmolloy.ie/ for a full description and follow-up descriptions.
-*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<errno.h>
@@ -22,27 +12,12 @@ int main(){
    int ret, fd;
    
    printf("Starting device test code example...\n");
+
    fd = open("/dev/akpchar", O_RDWR);             // Open the device with read/write access
    if (fd < 0){
       perror("Failed to open the device...");
       return errno;
    }
-
-   
-   char stringToSend[BUFFER_LENGTH];
-   
-   printf("Type in a short string to send to the kernel module:\n");
-   scanf("%s", stringToSend);                // Read in a string (with spaces)
-   printf("Writing message to the device [%s].\n", stringToSend);
-   ret = write(fd, stringToSend, strlen(stringToSend)); // Send the string to the LKM
-   if (ret < 0){
-      perror("Failed to write the message to the device.");
-      return errno;
-   }
-
-/*
-   printf("Press ENTER to read back from the device...\n");
-   getchar();*/
 
    printf("Reading from the device...\n");
    ret = read(fd, receive, BUFFER_LENGTH);        // Read the response from the LKM
@@ -51,6 +26,18 @@ int main(){
       return errno;
    }
    printf("The received message is: [%s]\n", receive);
+
+   char stringToSend[BUFFER_LENGTH];
+   
+   printf("Type in a short string to send to the kernel module:\n");
+   scanf("%s", stringToSend);                // Read in a string (with spaces)
+   printf("Writing message to the device [%s].\n", stringToSend);
+   ret = write(fd, stringToSend, strlen(stringToSend)); // Send the string to the LKM
+   if (ret < 0){
+      perror("Kernel module refused the message : ");
+      return errno;
+   }
+
    printf("End of the program\n");
    return 0;
 }
